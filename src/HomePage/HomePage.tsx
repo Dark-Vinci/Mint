9bcd9e6eaaf3b774089adc8cbc564bc1dbd59749
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-25 17:40:01 +0100

    chore: still on the setup

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,11 +1,14 @@
 import { JSX } from 'react';
 
 import style from './HomePage.module.scss';
+import { Nav } from '@components';
 
 export function HomePage(): JSX.Element {
     return (
         <div className={ style.container }>
-            HOMEPAGE
+            <nav>
+                <Nav />
+            </nav>
         </div>
     );
 }
