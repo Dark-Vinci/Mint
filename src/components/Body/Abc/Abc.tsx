d31a37c0354fb0f28832ece3515b700ee0b6cb28
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-26 19:57:38 +0100

    chore: Abc cleanups

--- src/components/Body/Abc/Abc.tsx
+++ src/components/Body/Abc/Abc.tsx
@@ -1,33 +1,8 @@
 import { JSX } from 'react';
-import { IoMdGlobe } from 'react-icons/io';
-import { FaThumbsUp } from 'react-icons/fa6';
-import { GrBlog } from 'react-icons/gr';
-import { PiChatsCircleDuotone } from 'react-icons/pi';
 import { GoKebabHorizontal } from 'react-icons/go';
 
 import style from './Abc.module.scss';
-
-const links = [
-  {
-    title: 'website',
-    icon: <IoMdGlobe size={'50px'} color="#838383" />,
-  },
-
-  {
-    title: 'social',
-    icon: <FaThumbsUp size={'50px'} color="#838383" />,
-  },
-
-  {
-    title: 'blog',
-    icon: <GrBlog size={'50px'} color="#838383" />,
-  },
-
-  {
-    title: 'chat',
-    icon: <PiChatsCircleDuotone size={'50px'} color="#838383" />,
-  },
-];
+import { links } from '@constants';
 
 export function Abc(): JSX.Element {
   return (
---
+++ src/constants/body.tsx
@@ -0,0 +1,26 @@
+import { IoMdGlobe } from 'react-icons/io';
+import { FaThumbsUp } from 'react-icons/fa6';
+import { GrBlog } from 'react-icons/gr';
+import { PiChatsCircleDuotone } from 'react-icons/pi';
+
+export const links = [
+  {
+    title: 'website',
+    icon: <IoMdGlobe size={'50px'} color="#838383" />,
+  },
+
+  {
+    title: 'social',
+    icon: <FaThumbsUp size={'50px'} color="#838383" />,
+  },
+
+  {
+    title: 'blog',
+    icon: <GrBlog size={'50px'} color="#838383" />,
+  },
+
+  {
+    title: 'chat',
+    icon: <PiChatsCircleDuotone size={'50px'} color="#838383" />,
+  },
+];
