823a7443ac34e46d6994949b3622600960b8ee68
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-27 13:12:05 +0100

    chore: Abc cleanups

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -3,12 +3,14 @@ import { JSX } from 'react';
 import style from './Body.module.scss';
 import { Hey } from './Hey';
 import { Abc } from './Abc';
+import { Need } from './Need';
 
 export function Body(): JSX.Element {
   return (
     <div className={style.container}>
       <Hey />
       <Abc />
+      <Need />
     </div>
   );
 }
